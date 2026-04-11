faf11c1691245079f259d069cb32a704bf80dc27
bu-okem <chiobiegbulem@gmail.com>
2026-04-11 18:20:07 +0100

    feat(grep-clone): implementation of core logic

--- grep-clone/src/main.rs
+++ grep-clone/src/main.rs
@@ -1,3 +1,25 @@
+use std::env::args;
+use std::fs::read_to_string;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = args().collect();
+
+    if args.len() < 3 {
+        eprintln!("Problem parsing arguments: not enough arguments");
+        exit(1);
+    }
+
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    let file = read_to_string(file_path).expect("Failed to read file");
+
+    let content = file.lines();
+
+    for line in content {
+        if line.to_lowercase().contains(&query.to_lowercase()) {
+            println!("Contained in: {}", line);
+        }
+    }
 }
