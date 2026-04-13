deed5a7762cb672fb4002c175a8dd32b241e12fc
bu-okem <chiobiegbulem@gmail.com>
2026-04-13 18:39:31 +0100

    feat(grep-clone): structuring

--- grep-clone/src/main.rs
+++ grep-clone/src/main.rs
@@ -1,25 +1,51 @@
 use std::env::args;
+use std::error::Error;
 use std::fs::read_to_string;
 use std::process::exit;
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            eprintln!("Problem parsing arguments: not enough arguments");
+        }
+        Ok(Config {
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+        })
+    }
+}
+
+fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+    let file = read_to_string(&config.file_path)?;
+
+    let content = file.lines();
+
+    let query = &config.query.to_lowercase();
+
+    for line in content {
+        if line.to_lowercase().contains(query) {
+            println!("Contained in: {}", line);
+        }
+    }
+
+    Ok(())
+}
 
 fn main() {
     let args: Vec<String> = args().collect();
 
-    if args.len() < 3 {
-        eprintln!("Problem parsing arguments: not enough arguments");
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
         exit(1);
-    }
+    });
 
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    let file = read_to_string(file_path).expect("Failed to read file");
-
-    let content = file.lines();
-
-    for line in content {
-        if line.to_lowercase().contains(&query.to_lowercase()) {
-            println!("Contained in: {}", line);
-        }
+    if let Err(e) = run(&config) {
+        eprintln!("Application error: {}", e);
+        exit(1);
     }
 }
